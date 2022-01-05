133431faf5764cd704d521120d7e6484b16339c2
TWAHIRWA Dieudonné <twahirwa25@gmail.com>
2022-01-05 18:36:29 +0100

    up 4

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# Install yarn
-# /opt/plesk/node/14/bin/npm install yarn --scripts-prepend-node-path
-
-# Install with npm
-#/opt/plesk/node/14/bin/npm install --scripts-prepend-node-path
-
-# Install with yarn
-# /var/www/vhosts/centrefamilial.tk/centrefamilial.be/backend/node_modules/yarn/bin/yarn install
-
 # Install with npm CI
 /opt/plesk/node/14/bin/npm ci --scripts-prepend-node-path
 
