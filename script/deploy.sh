3c72eebcde4fee343e3d92237fab844eedd985fa
TWAHIRWA Dieudonné <twahirwa25@gmail.com>
2022-01-05 17:49:34 +0100

    up 3

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-# Install dependecies using yarn
-/opt/plesk/node/14/bin/npm install yarn --scripts-prepend-node-path
+# Install yarn
+# /opt/plesk/node/14/bin/npm install yarn --scripts-prepend-node-path
 
-# Build project
-/var/www/vhosts/centrefamilial.tk/centrefamilial.be/backend/node_modules/yarn/bin/yarn install
+# Install with npm
+#/opt/plesk/node/14/bin/npm install --scripts-prepend-node-path
+
+# Install with yarn
+# /var/www/vhosts/centrefamilial.tk/centrefamilial.be/backend/node_modules/yarn/bin/yarn install
+
+# Install with npm CI
+/opt/plesk/node/14/bin/npm ci --scripts-prepend-node-path
+
+# Build project with npm
+NODE_ENV=production /opt/plesk/node/14/bin/npm run build --scripts-prepend-node-path
 
 # Restart node app
 touch ./tmp/restart.txt
