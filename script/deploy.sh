c8f0553ba0995e1a1e6ab9991bd2df53ca008105
TWAHIRWA Dieudonné <twahirwa25@gmail.com>
2022-01-09 17:44:05 +0100

    up back

--- script/deploy.sh
+++ script/deploy.sh
@@ -8,3 +8,12 @@ NODE_ENV=production /opt/plesk/node/14/bin/npm run build --scripts-prepend-node-
 
 # Restart node app
 touch ./tmp/restart.txt
+
+#!/bin/bash
+
+# Install with npm CI
+# /opt/plesk/node/14/bin/npm install --scripts-prepend-node-path
+# /opt/plesk/node/14/bin/npm ci --scripts-prepend-node-path
+
+# Build project with npm
+# NODE_ENV=production /opt/plesk/node/14/bin/npm run build --scripts-prepend-node-path
